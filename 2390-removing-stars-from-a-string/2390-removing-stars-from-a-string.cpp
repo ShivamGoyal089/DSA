4a914166abdf0cf42725a25d1c594929d647bb71
Shivam Goyal <shivamgoyal1616@gmail.com>
2026-09-15 16:46:21 +0530

    Time: 14 ms (79.57%), Space: 28.2 MB (87.48%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans = "";
+        for(char ch : s){
+            if(ch == '*'){
+                if(!ans.empty()){
+                    ans.pop_back();
+                }
+        
+            }
+            else{
+                ans.push_back(ch);
+            }
+        }
+    return ans;
+        
+    }
+};
